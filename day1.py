be6dbf3789562ef67c21d65d1dcf8db4cb11f3cc
Maheshkota2627 <126803011+Maheshkota2627@users.noreply.github.com>
2024-02-20 15:08:54 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,87 @@
+'''tp=int(input());ta=int(input())
+print(ta/ap)..'''
+
+
+'''a=int(input('enter the number'))
+if a%2==0 :
+      print('it is even')
+else:
+    print('it is odd')'''
+
+'''b=int(input('enter the valueof b'))
+a=int(input('enter the value of a'))
+if a>b:
+    print('it very big')
+elif a<b:
+    print('it very small')
+else :
+    print('it eqaul')   '''
+
+
+'''
+
+#it is very intrusting program
+x,y= map(float,input().split(" "))   #x = withdraw,y = initial
+if y>x and x%5==0:
+    print(y-x)
+else:
+    print('insufisiant money',y)'''
+
+'''
+
+# toy cost is 100, discount is x,
+
+t,x= map(float,input("toy and discount ").split(" "))
+print('selling',t-(t*(x/100)))'''
+
+'''
+tv1,tv2,d1,d2= map(float,input("tv1 tv2 d1 d2").split(" "))
+to1 = tv1-(tv1*(d1/100))
+to2 = tv2-(tv2*(d2/100))
+if to1>to2:
+    print('tv2 is better')
+elif to1<to2:
+    print('tv1 is better')
+else :
+    print('both are same')
+'''
+'''
+a = int(input('enter year'))
+if a%4==0 and (a%100 !=0 and a%400==0) :
+    print('it is leap year')
+else :
+    print('it is not leap year')
+'''
+
+#you have `12 cocos and your  friend are 20 then howmuch need
+cho,fr= map(float,input("cho fr").split(" "))
+if cho > fr :
+    print('remaining',fr-cho)
+    
+elif cho < fr :
+    print('need',fr-cho)
+else :
+    print('it is enough')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
